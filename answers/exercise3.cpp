fe82b107149e097181e91f5b00a0e34113ff7b7e
Thomas Yeh <tyeh@broadwayedu.com>
2012-09-07 11:35:03 -0700

    Initial commit

---
+++ answers/exercise3.cpp
@@ -0,0 +1,24 @@
+// exercise3.cpp : Defines the entry point for the console application.
+//
+
+
+#include "stdafx.h"
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+
+// define input and output functions
+
+void main()                               // define main program
+{
+   
+  int index, a;
+  for (index=1; index <=128; index++) {
+	  if (index%10)
+		  cout << (char) index << ' ';
+	  else
+		cout << (char) index << '\n';
+  }
+   cout << "Enter any number to exit:";       // tell user how to get out. 
+   cin >> a;                              // wait for input before exit.
+}                
