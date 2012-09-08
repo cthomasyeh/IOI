bcbfa5a35ad969faa596ca5f9c880b3cf6ea4f5f
Thomas Yeh <tyeh@broadwayedu.com>
2012-09-08 15:49:20 -0700

    exercise 9

--- answers/exercise7_2.cpp
+++ answers/exercise7_2.cpp
@@ -12,24 +12,28 @@ using namespace std;
 void main()                               // define main program
 {
    
-  int studentAge[12];
+  int studentAge[12] = {10, 11, 12, 13, 15, 20, 8, 9, 12, 25, 30, 6};
   int i, a;
   int greater12=0;
   int oldest=0;
   int youngest=100;
+  int rank = 0;
 
-  for (i=0; i<12; i++) cin >> studentAge[i];
+  //for (i=0; i<12; i++) cin >> studentAge[i];
 
   for (i=0; i<12; i++) {
 	  if (studentAge[i] >= 12) greater12 = greater12 + 1;
-	  if (studentAge[i] > oldest) oldest = studentAge[i];
+	  if (studentAge[i] > oldest) {
+		  oldest = studentAge[i];
+		  rank = i;
+	  }
 	  if (studentAge[i] < youngest) youngest = studentAge[i];
 
   }
 
   cout << "size of array: " << sizeof(studentAge) << '\n';
   cout << "Students older than 12: " << greater12 << '\n';
-  cout << "Oldest student age is: " << oldest << '\n';
+  cout << "Oldest student age is: " << oldest << " oldest student is in: " << rank + 1 <<"th position\n";
   cout << "Youngest student age is: " << youngest << '\n';
 
   cout << "\nEnter any number to exit:";       // tell user how to get out. 
--- exercise7.cpp
+++ exercise7.cpp
@@ -22,7 +22,10 @@ void main()                               // define main program
 
   cout << '\n';
 
-  for (i=0; i<12; i++) cout << studentAge[i] << '\n';
+  //for (i=0; i<12; i++) cout << studentAge[i] << '\n';
+  cout << "First element: " << studentAge[0] << '\n';
+  cout << "6th element: " << studentAge[5] << '\n';
+  cout << "last element: " << studentAge[11];
 
   cout << "\nEnter any number to exit:";       // tell user how to get out. 
   cin >> a;                              // wait for input before exit.
