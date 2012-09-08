2aa73b0ee915142bfac8bb6affa23755f9d6b16c
Thomas Yeh <tyeh@broadwayedu.com>
2012-09-08 11:29:16 -0700

    exercise 7_1

--- answers/exercise7_1.cpp
+++ answers/exercise7_1.cpp
@@ -15,22 +15,18 @@ void main()                               // define main program
   int studentAge[12];
   int i, a;
   int greater12=0;
-  int oldest=0;
-  int youngest=100;
+  
 
   for (i=0; i<12; i++) cin >> studentAge[i];
 
   for (i=0; i<12; i++) {
-	  if (studentAge[i] >= 12) greater12 = greater12 + 1;
-	  if (studentAge[i] > oldest) oldest = studentAge[i];
-	  if (studentAge[i] < youngest) youngest = studentAge[i];
+	  if (studentAge[i] >= 12) greater12 = greater12 + 1;	  
 
   }
 
-  cout << "size of array: " << sizeof(studentAge) << '\n';
+  //cout << "size of array: " << sizeof(studentAge) << '\n';
   cout << "Students older than 12: " << greater12 << '\n';
-  cout << "Oldest student age is: " << oldest << '\n';
-  cout << "Youngest student age is: " << youngest << '\n';
+ 
 
   cout << "\nEnter any number to exit:";       // tell user how to get out. 
   cin >> a;                              // wait for input before exit.
