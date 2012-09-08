c692058772d6468cb234933e4df9d31cb3c3011a
Thomas Yeh <tyeh@broadwayedu.com>
2012-09-08 11:20:15 -0700

    exercise 7 begins here

--- exercise7.cpp
+++ exercise7.cpp
@@ -20,17 +20,11 @@ void main()                               // define main program
 
   for (i=0; i<12; i++) cin >> studentAge[i];
 
-  for (i=0; i<12; i++) {
-	  if (studentAge[i] >= 12) greater12 = greater12 + 1;
-	  if (studentAge[i] > oldest) oldest = studentAge[i];
-	  if (studentAge[i] < youngest) youngest = studentAge[i];
+  cout << '\n';
 
-  }
-
-  cout << "Students older than 12: " << greater12 << '\n';
-  cout << "Oldest student age is: " << oldest << '\n';
-  cout << "Youngest student age is: " << youngest << '\n';
+  for (i=0; i<12; i++) cout << studentAge[i] << '\n';
 
   cout << "\nEnter any number to exit:";       // tell user how to get out. 
   cin >> a;                              // wait for input before exit.
+
 }                
